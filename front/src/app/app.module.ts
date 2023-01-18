9360b77a06a3b295a96bc790f5fa13b7ad6a9f98
kiady <kiadyrandria07@gmail.com>
2023-01-18 22:02:34 +0300

    LOGIN (FRONT) IN PROGRESS

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { LoginComponent } from './components/login/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
