8af0277d0ff7305430d61a336f3da8fb3107cde8
kiady <kiadyrandria07@gmail.com>
2023-01-24 23:34:03 +0300

    login in progress

--- front/src/app/demo/pages/authentication/login/login.component.ts
+++ front/src/app/demo/pages/authentication/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Router } from 'express';
+import { FormsModule, NgModel } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -8,5 +12,27 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
+
+
 })
-export default class LoginComponent {}
+export default class LoginComponent {
+  email: NgModel;
+  password: string;
+  private router: Router;
+  private http: HttpClient;
+
+
+  login() {
+    const body = { username: this.email, password: this.password };
+    this.http.post('http://localhost:3000/users/login', body).subscribe(
+      res => {
+        console.log(res);
+        // this.router.navigateByUrl(['/default']);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+}
