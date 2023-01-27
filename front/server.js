d84d93ff315cd7db778396a381f6d062e63b0e5b
Kiady Mandresy <kiady.randria@pulse.mg>
2023-01-27 16:56:42 +0300

    deploy front SCRIPT

---
+++ front/server.js
@@ -0,0 +1,16 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+const portS = (process.env.PORT || 4200);
+// Start the app by listening on the default Heroku port
+console.log("listening on ", portS);
+app.listen(process.env.PORT || 4200);
